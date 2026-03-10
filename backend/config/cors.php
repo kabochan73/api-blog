9bc9996c2bbaa03f834a9bffa91ae666682edf1e
kabochan73 <kabochan7355@gmail.com>
2026-03-10 19:34:28 +0900

    feat: CORSの設定を追加

---
+++ backend/config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        env('FRONTEND_URL', 'https://api-blog-psi.vercel.app'),
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
